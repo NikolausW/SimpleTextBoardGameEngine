18dd1fb0ef8c6e93e2c115b2543a73569249d14c
Nikolaus <NikolausWood@gmail.com>
2019-11-14 16:36:09 -0800

    Tic Tac Toe PlaySpace implemented

--- Games/TicTacToe/TTTPlaySpace.h
+++ Games/TicTacToe/TTTPlaySpace.h
@@ -10,6 +10,7 @@ namespace UserInterface
    {
       public:
          TicTacToe();
+         virtual void Print_PlaySpace(void);
       protected:
          virtual void Write_Board(void);
          virtual void Write_Header(void);
@@ -28,10 +29,13 @@ namespace UserInterface
          //Unique Methods
          void setupCircle(void);
          void setupCross(void); 
-                                 
+         void Clear_Board(void);
          void Write_Blank(int square);
          void Write_Cross(int square);
          void Write_Circle(int square);
+         void Write_Redo(bool available);
+         void Write_Undo(bool available);
+         size_t Get_Coordinate(int square);
    };
 }
 #endif
